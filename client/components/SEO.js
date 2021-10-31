a12dbd6a55c7a09014ddeffdc71bd77b7ee4a66c
khacvy <khacvy93@gmail.com>
2021-10-31 16:11:42 +0700

    chore: update SEO

--- client/components/SEO.js
+++ client/components/SEO.js
@@ -2,7 +2,7 @@ import Head from "next/head";
 import { useRouter } from "next/router";
 
 const description =
-  "Meta Pumpkin are a collection of 147 randomly generated and unique creatures living on the BSC blockchain in the form of ERC-721 tokens";
+  "MetaPumpkin is a collection of 147 randomly generated and unique creatures living on the BSC blockchain in the form of ERC-721 tokens";
 
 const CommonSEO = () => {
   const router = useRouter();
@@ -19,11 +19,19 @@ const CommonSEO = () => {
       <meta property="og:site_name" content="MetaPumpkins" />
       <meta property="og:description" content={description} />
       <meta property="og:title" content="MetaPumpkins" />
+      <meta
+        property="og:image"
+        content={`${process.env.APP_URL}/twitter-card.png`}
+      />
+
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:site" content="https://twitter.com/trankhac_vy" />
       <meta name="twitter:title" content="MetaPumpkins" />
       <meta name="twitter:description" content={description} />
-      <meta name="twitter:image" content={`${process.env.APP_URL}/twitter-card.png`} />
+      <meta
+        name="twitter:image"
+        content={`${process.env.APP_URL}/twitter-card.png`}
+      />
     </Head>
   );
 };
